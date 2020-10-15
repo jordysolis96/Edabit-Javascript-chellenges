ef751d5b3df47a3550456ef7c0537eba6cee22bb
jordy muniz <jordy.muniz96@gmail.com>
2020-10-14 23:18:38 -0500

    first commit

---
+++ Javascript/edabit.js
@@ -0,0 +1,13 @@
+// Create a function that takes two numbers as arguments and return their sum
+function addition(a, b){
+    return a + b;
+}
+// Create a function that takes the age and return the age in days.
+function calcAge(age){
+    return age * 365;
+}
+// Write a function that converts hours into seconds
+function howManySeconds(hours) {
+    return hours * 3600;
+}
+
