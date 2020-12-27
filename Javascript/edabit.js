3453052b2e5155f0c48b02ed6a925dc697d237c5
jordy muniz <jordy.muniz96@gmail.com>
2020-12-26 23:21:00 -0600

    triangle exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -36,3 +36,8 @@ function lessThanOrEqualToZero(num) {
 function remainder(x, y) {
     return x % y;
 }
+
+//Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+function nextEdge(side1, side2) {
+    return (side1 + side2) - 1
+}
