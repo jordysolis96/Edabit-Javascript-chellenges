986c341e0329ff624d8f6d80d6e6bbc8dd5fa5a3
jordy muniz <jordy.muniz96@gmail.com>
2020-12-26 23:44:40 -0600

    basketball  exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -51,3 +51,8 @@ function circuitPower(voltage, current) {
 function increment(num) {
     return ++num;
 }
+
+//You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+function points(twoPointers, threePointers){
+    return twoPointers * 2 + threePointers * 3;
+}
