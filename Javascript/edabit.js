0a8b7263e5fa8ee12b261a15b44490040bf346fd
jordy muniz <jordy.muniz96@gmail.com>
2020-12-26 23:55:06 -0600

    exercise complete

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -56,3 +56,10 @@ function increment(num) {
 function points(twoPointers, threePointers){
     return twoPointers * 2 + threePointers * 3;
 }
+
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+function lessThan100(a, b) {
+    if((a + b) < 100){
+        return true;
+    }
+}
