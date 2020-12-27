14abbb17cc5807e4353e05e32544ab08dd0a8443
jordy muniz <jordy.muniz96@gmail.com>
2020-12-26 23:05:05 -0600

    edabit exercise complete

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -12,7 +12,22 @@ function howManySeconds(hours) {
     return hours * 3600;
 }
 
-//write a funtion that converts minute into seconds
+//write a function that converts minute into seconds
 function Connvert(Minutes) {
     return minutes * 60;
+}
+
+//Create a function that takes an array containing only numbers and return the first element
+function getFirstValue(arry){
+   var randomArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    console.log(ra)
+}
+
+//Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false
+function lessThanOrEqualToZero(num) {
+    if(num <= 0){
+        return true;
+    } else {
+        return false;
+    }
 }
