328c712d22b9866e0976a7f62e430321937bf80a
jordy muniz <jordy.muniz96@gmail.com>
2020-12-26 23:15:28 -0600

    modulus operator comp;ete

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -31,3 +31,8 @@ function lessThanOrEqualToZero(num) {
         return false;
     }
 }
+
+//There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+function remainder(x, y) {
+    return x % y;
+}
