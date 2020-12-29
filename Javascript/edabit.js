c246d5279746f96919cca4a3187205676b3c85e7
jordy muniz <jordy.muniz96@gmail.com>
2020-12-28 23:33:47 -0600

    syntax error exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -102,3 +102,8 @@ function animals(chickens, cows, pigs) {
 function concatName(firstName, lastName) {
     return lastName + ", " + firstName;
 }
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+function cubes(a) {
+    return a * 3
+}
