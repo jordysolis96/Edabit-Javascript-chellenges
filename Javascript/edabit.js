d709ef80a98a99564942652d25975dcd3c92f1b1
jordy muniz <jordy.muniz96@gmail.com>
2020-12-29 18:32:34 -0600

    operators exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -175,3 +175,12 @@ function getVoteCount(votes) {
 function secondLargest(arr) {
     return arr.sort((a,b) => +a - +b )[arr.length-2]
 }
+
+//Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+function lessThanOrEqualToZero(num) {
+    if(num <= 0){
+        return true
+    } else{
+        return false
+    }
+}
