610961d0fd6d1d99f1a94b74c41957b18d7ff5a5
jordy muniz <jordy.muniz96@gmail.com>
2020-12-29 18:36:10 -0600

    debugging exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -184,3 +184,11 @@ function lessThanOrEqualToZero(num) {
         return false
     }
 }
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+// function squaed(b) {
+//     return a * a
+// }
+function squared(b) {
+    return b * b;
+}
