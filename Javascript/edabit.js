8bd14467d0f6e3576fac140f9fa62cf241e47147
jordy muniz <jordy.muniz96@gmail.com>
2020-12-29 18:37:05 -0600

    refactored exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -105,7 +105,7 @@ function concatName(firstName, lastName) {
 
 //Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
 function cubes(a) {
-    return a * 3
+    return a * a * a;
 }
 
 //Create a function that returns True if a string is empty and False otherwise
