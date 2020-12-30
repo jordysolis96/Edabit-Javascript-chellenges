8b642a392a2cb44bf0066d1a94c70a5da1b8ad88
jordy muniz <jordy.muniz96@gmail.com>
2020-12-29 18:41:47 -0600

    math exercise

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -192,3 +192,12 @@ function lessThanOrEqualToZero(num) {
 function squared(b) {
     return b * b;
 }
+
+//Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+function makesTen(a, b) {
+    if(a + b === 10){
+        return true;
+    } else {
+        return false
+    }
+}
