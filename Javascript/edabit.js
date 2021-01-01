6b122ff3800a227b7b5954ac9280f955dd35663a
jordy muniz <jordy.muniz96@gmail.com>
2020-12-31 23:56:40 -0600

    array exercise complete

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -201,3 +201,8 @@ function makesTen(a, b) {
         return false
     }
 }
+
+//Create a function that returns an array of strings sorted by length in ascending order.
+function sortByLength(arr) {
+    return arr.sort((a, b) => a.length - b.length);
+}
