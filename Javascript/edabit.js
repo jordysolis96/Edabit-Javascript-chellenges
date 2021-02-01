22eddd85ad35b25d90fe1475d363592b6df2d7ed
jordy muniz <jordy.muniz96@gmail.com>
2021-01-31 23:54:22 -0600

    added new problem

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -216,3 +216,8 @@ function vowelLinks(str) {
 function getHashTags(str) {
     return str.split(' ').sort((a, b) => b.length - a.length).slice(0, 3).map(c => '#' + c.replace(/\W/, '').toLowerCase());
 }
+
+//Create a function that takes a word and returns the new word without including the first character
+function newWord(str) {
+    return str.slice(1);
+}
