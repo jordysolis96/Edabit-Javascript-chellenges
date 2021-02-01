673da5f1175ed4d9663b56cd3cbcb63ea8c01da6
jordy muniz <jordy.muniz96@gmail.com>
2021-01-31 23:59:17 -0600

    problem

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -231,3 +231,8 @@ function AlphabetSoup(str) {
 function lettersOnly(str) {
     return str.replace(/[^a-z]/gi, '');
 }
+
+//Create a function that takes any nonnegative number as an argument and return it with it's digits in descending order. Descending order is when you sort from highest to lowest.
+function sortDescending(num) {
+    return parseInt(num.toString().split('').sort().reverse().join(''));
+}
