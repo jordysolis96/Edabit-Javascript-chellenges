7254efc6bc924069ff079ac8d83766bfaf29d725
jordy muniz <jordy.muniz96@gmail.com>
2021-02-08 11:47:36 -0600

    return odd number in a array

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -295,3 +295,24 @@ function findNemo(sentence) {
     }
 
 }
+
+//Given the array array of var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function isOdd(x){
+    if(x % 2 !== 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function oddArr(arr){
+    var newArr =[]
+    for(var i =0; i< arr.length; i++) {
+        if (isOdd(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    console.log(newArr)
+}
+
