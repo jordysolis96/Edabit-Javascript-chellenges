d6a29cb1a71690315902f547d29e1aede6e1da1f
jordy muniz <jordy.muniz96@gmail.com>
2021-02-07 19:24:33 -0600

    Fining Nemo

--- Javascript/edabit.js
+++ Javascript/edabit.js
@@ -283,3 +283,15 @@ function creditCard(card){
     }
     return card.join('');
 }
+
+//You're given a string of words. You need to find the word "Nemo", and return a string like this: "I found Nemo at [the order of the word you find nemo]!".
+//
+// If you can't find Nemo, return "I can't find Nemo :(".
+function findNemo(sentence) {
+    if (sentence.includes("Nemo ")){
+        return "I found Nemo at " + (sentence.split(" ").indexOf("Nemo")+1)+ "!"
+    }else{
+        return "I can't find Nemo :("
+    }
+
+}
